bfccbc1824dae84e683ff9e00da48aa8a3291ffb
Josh Lubaway <josh.lubaway@gmail.com>
2015-01-25 11:33:07 -0800

    Challenge 5.
    Looping over any number of elevators.

--- elevator_saga.js
+++ elevator_saga.js
@@ -1,25 +1,19 @@
 {
   init: function(elevators, floors) {
-    var elevator1 = elevators[0];
-    var elevator2 = elevators[1];
 
     function randomFloor() {
       return Math.floor(Math.random() * elevators.length);
     }
 
-    elevator1.on("idle", function() {
-      elevator1.goToFloor(randomFloor());
-    });
-    elevator1.on('floor_button_pressed', function(floor) {
-      elevator1.goToFloor(floor);
+    elevators.forEach(function(elevator) {
+      elevator.on('idle', function() {
+        elevator.goToFloor(randomFloor());
+      });
+      elevator.on('floor_button_pressed', function(floor) {
+        elevator.goToFloor(floor);
+      });
     });
 
-    elevator2.on("idle", function() {
-      elevator2.goToFloor(randomFloor());
-    });
-    elevator2.on('floor_button_pressed', function(floor) {
-      elevator2.goToFloor(floor);
-    });
   },
   update: function(dt, elevators, floors) {
     // We normally don't need to do anything here
