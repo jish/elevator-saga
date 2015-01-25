9a8e2836acca4c725d5fdee401ee9c16a8d70271
Josh Lubaway <josh.lubaway@gmail.com>
2015-01-25 11:21:48 -0800

    Challenge 3.

--- elevator_saga.js
+++ elevator_saga.js
@@ -1,12 +1,22 @@
 {
   init: function(elevators, floors) {
-    var elevator = elevators[0]; // Let's use the first elevator
-    elevator.on("idle", function() {
-      elevator.goToFloor(0);
-      elevator.goToFloor(1);
-      elevator.goToFloor(2);
-      elevator.goToFloor(3);
-      elevator.goToFloor(4);
+    var elevator1 = elevators[0];
+    var elevator2 = elevators[1];
+
+    elevator1.on("idle", function() {
+      elevator1.goToFloor(0);
+      elevator1.goToFloor(1);
+      elevator1.goToFloor(2);
+      elevator1.goToFloor(3);
+      elevator1.goToFloor(4);
+    });
+
+    elevator2.on("idle", function() {
+      elevator2.goToFloor(4);
+      elevator2.goToFloor(3);
+      elevator2.goToFloor(2);
+      elevator2.goToFloor(1);
+      elevator2.goToFloor(0);
     });
   },
   update: function(dt, elevators, floors) {
